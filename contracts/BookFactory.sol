e7f80f9fa3092a906552cf0ff2e334a0f5388bb8
Kristiyan Dimitrov <dimitkris@gmail.com>
2021-09-17 11:26:26 +0300

    Change book id generation to 256bit by title, add duplicate book check on add

--- contracts/BookFactory.sol
+++ contracts/BookFactory.sol
@@ -4,29 +4,27 @@ pragma solidity >=0.8.6;
 import "../contracts/Ownable.sol";
 
 contract BookFactory is Ownable {
-    uint bookIdCounter;
-
     struct Book {
-        uint id;
+        bytes32 id;
         string title;
         uint numberOfCopies;
         address[] borrowedUsersHistory;
     }
 
     // events
-    event BookAdded(uint id, string title, uint numberOfCopies);
-    event BookBorrowed(uint id, address user);
-    event BookReturned(uint id, address user);
+    event BookAdded(bytes32 id, string title, uint numberOfCopies);
+    event BookBorrowed(bytes32 id, address user);
+    event BookReturned(bytes32 id, address user);
 
     // storage and mapping
-    uint[] private bookIds;
-    mapping(uint => Book) books;
-    mapping(uint => address[]) bookToCurrentBorrowedUsers;
-    mapping(address => mapping(uint => bool)) userToBorrowedBooks;
+    bytes32[] private bookIds;
+    mapping(bytes32 => Book) books;
+    mapping(bytes32 => address[]) bookToCurrentBorrowedUsers;
+    mapping(address => mapping(bytes32 => bool)) userToBorrowedBooks;
 
-    function addBook(string memory _title, uint _numberOfCopies) public onlyOwner {        
+    function addBook(string memory _title, uint _numberOfCopies) public onlyOwner checkIfBookExists(_title) {        
         address[] memory borrowHistory;
-        uint bookId = _getId();
+        bytes32 bookId = _getId(_title);
         Book memory newBook = Book(bookId, _title, _numberOfCopies, borrowHistory);
         bookIds.push(bookId);
         books[bookId] = newBook;
@@ -34,7 +32,7 @@ contract BookFactory is Ownable {
         emit BookAdded(bookId, _title, _numberOfCopies);
     }
 
-    function borrowBook(uint _bookId) public {
+    function borrowBook(bytes32 _bookId) public {
         require(userToBorrowedBooks[msg.sender][_bookId] == false, "User has already borrowed the book.");     
         require(books[_bookId].numberOfCopies > 0, "No available copies of the book.");
 
@@ -46,7 +44,7 @@ contract BookFactory is Ownable {
         emit BookBorrowed(_bookId, msg.sender);
     }
 
-    function returnBook(uint _bookId) public {
+    function returnBook(bytes32 _bookId) public {
         require(userToBorrowedBooks[msg.sender][_bookId] == true, "User has not borrowed this book.");     
 
         books[_bookId].numberOfCopies++;
@@ -57,7 +55,7 @@ contract BookFactory is Ownable {
     function getAvailableBooks() public view returns (Book[] memory) {
         uint availableBooksCount;
         for (uint i = 0; i < bookIds.length; i++) {
-            uint bookId = bookIds[i];
+            bytes32 bookId = bookIds[i];
 
             if (books[bookId].numberOfCopies > 0) {
                 availableBooksCount++;
@@ -70,7 +68,7 @@ contract BookFactory is Ownable {
 
         Book[] memory availableBooks = new Book[](availableBooksCount);
         for (uint i = 0; i < bookIds.length; i++) {
-            uint bookId = bookIds[i];
+            bytes32 bookId = bookIds[i];
 
             if (books[bookId].numberOfCopies > 0) {
                 availableBooks[i] = books[bookId];
@@ -80,16 +78,18 @@ contract BookFactory is Ownable {
         return availableBooks;
     }
 
-    function getBookBorrowers(uint _bookId) public view returns (address[] memory) {
+    function getBookBorrowers(bytes32 _bookId) public view returns (address[] memory) {
         return books[_bookId].borrowedUsersHistory;
     }
 
-    function _getId() private returns (uint) {
-        return bookIdCounter++;
+    function _getId(string memory _title) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_title));
     }
 
     modifier checkIfBookExists(string memory _title) {
-        // add implementation
+        bytes32 bookId = _getId(_title);
+        Book memory book = books[bookId];
+        require(bytes(book.title).length == 0, "Book already added.");
         _;
     }
 }
