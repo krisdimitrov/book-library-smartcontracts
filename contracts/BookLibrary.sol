04e91cd93a5182658737fda6c56b9f331c453ce2
krisdimitrov <dimitkris@gmail.com>
2021-10-04 14:14:04 +0300

    Add OpenZepellin dependency and use Ownable

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4;
 
-import "../contracts/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BookLibrary is Ownable {
     struct Book {
