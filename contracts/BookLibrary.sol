ea213cee9f862b40af4b9aa4b20c38f10a769b53
krisdimitrov <dimitkris@gmail.com>
2021-10-12 17:55:34 +0300

    Clean up code, refactor more functions to use modifiers

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BookLibrary is Ownable {
     struct Book {
-        bytes32 id;
         string title;
         uint256 copies;
         address[] borrowedUsersHistory;
@@ -17,116 +16,111 @@ contract BookLibrary is Ownable {
     event BookReturned(bytes32 id, string title, address user);
 
     // storage and mapping
-    bytes32[] private bookIds;
-    mapping(bytes32 => Book) books;
-    mapping(bytes32 => address[]) bookToCurrentBorrowedUsers;
+    bytes32[] public bookIds;
+    mapping(bytes32 => Book) public books;
     mapping(address => mapping(bytes32 => bool)) userToBorrowedBooks;
+
+    modifier validateInputs(string memory _title, uint256 _copies) {
+        require(bytes(_title).length > 0, "Title should not be empty.");
+        require(_copies > 0, "Copies should be greater than 0.");
+        _;
+    }
+
+    modifier checkBookExists(string memory _title, bool shouldExist) {
+        bytes32 bookId = _getId(_title);
+        if (shouldExist) {
+            require(
+                bytes(books[bookId].title).length > 0,
+                "Book does not exist."
+            );
+        } else {
+            require(
+                bytes(books[bookId].title).length == 0,
+                "Book already exists."
+            );
+        }
+        _;
+    }
 
     function addBook(string memory _title, uint256 _copies)
         public
         onlyOwner
         validateInputs(_title, _copies)
+        checkBookExists(_title, false)
     {
         address[] memory borrowHistory;
         bytes32 bookId = _getId(_title);
-        Book memory newBook = Book(bookId, _title, _copies, borrowHistory);
+        Book memory newBook = Book(_title, _copies, borrowHistory);
+
         bookIds.push(bookId);
         books[bookId] = newBook;
 
         emit BookAdded(bookId, _title, _copies);
     }
 
-    function borrowBook(bytes32 _bookId) public {
+    function borrowBook(string memory _title)
+        public
+        checkBookExists(_title, true)
+    {
+        bytes32 bookId = _getId(_title);
+        Book storage bookToBorrow = books[bookId];
+
         require(
-            userToBorrowedBooks[msg.sender][_bookId] == false,
+            userToBorrowedBooks[msg.sender][bookId] == false,
             "User has already borrowed the book."
         );
-        require(books[_bookId].copies > 0, "No available copies of the book.");
+        require(bookToBorrow.copies > 0, "No available copies of the book.");
 
-        Book storage bookToBorrow = books[_bookId];
         bookToBorrow.copies--;
         bookToBorrow.borrowedUsersHistory.push(msg.sender);
-        userToBorrowedBooks[msg.sender][_bookId] = true;
+        userToBorrowedBooks[msg.sender][bookId] = true;
 
-        emit BookBorrowed(_bookId, bookToBorrow.title, msg.sender);
+        emit BookBorrowed(bookId, bookToBorrow.title, msg.sender);
     }
 
-    function returnBook(bytes32 _bookId) public {
+    function returnBook(string memory _title)
+        public
+        checkBookExists(_title, true)
+    {
+        bytes32 bookId = _getId(_title);
+        Book storage bookToReturn = books[bookId];
+
         require(
-            userToBorrowedBooks[msg.sender][_bookId] == true,
+            userToBorrowedBooks[msg.sender][bookId] == true,
             "User has not borrowed this book."
         );
 
-        books[_bookId].copies++;
-        userToBorrowedBooks[msg.sender][_bookId] = false;
+        bookToReturn.copies++;
+        userToBorrowedBooks[msg.sender][bookId] = false;
 
-        emit BookReturned(_bookId, books[_bookId].title, msg.sender);
+        emit BookReturned(bookId, bookToReturn.title, msg.sender);
     }
 
-    function getAvailableBooks() public view returns (Book[] memory) {
-        uint256 availableBooksCount = 0;
-        for (uint256 i = 0; i < bookIds.length; i++) {
-            bytes32 bookId = bookIds[i];
-
-            if (books[bookId].copies > 0) {
-                availableBooksCount++;
-            }
-        }
-
-        if (availableBooksCount == 0) {
-            return new Book[](0);
-        }
-
-        Book[] memory availableBooks = new Book[](availableBooksCount);
-        uint256 k = 0;
-        for (uint256 i = 0; i < bookIds.length; i++) {
-            bytes32 bookId = bookIds[i];
-
-            if (books[bookId].copies > 0) {
-                availableBooks[k] = books[bookId];
-                k++;
-            }
-        }
-
-        return availableBooks;
+    function getBook(string memory _title)
+        external
+        view
+        checkBookExists(_title, true)
+        returns (Book memory)
+    {
+        bytes32 bookId = _getId(_title);
+        return books[bookId];
     }
 
-    function getAllBooks() public view returns (Book[] memory) {
-        Book[] memory allBooks = new Book[](bookIds.length);
-
-        for (uint256 i = 0; i < bookIds.length; i++) {
-            bytes32 bookId = bookIds[i];
-            allBooks[i] = books[bookId];
-        }
-
-        return allBooks;
+    function getBooksCount() external view returns (uint256) {
+        return bookIds.length;
     }
 
-    function getBookBorrowers(bytes32 _bookId)
+    function getBookBorrowers(string memory _title)
         public
         view
+        checkBookExists(_title, true)
         returns (address[] memory)
     {
-        require(_checkBookExists(_bookId), "Book does not exist.");
-        return books[_bookId].borrowedUsersHistory;
+        bytes32 bookId = _getId(_title);
+        return books[bookId].borrowedUsersHistory;
     }
 
     function _getId(string memory _title) private pure returns (bytes32) {
         return keccak256(abi.encodePacked(_title));
-    }
-
-    function _checkBookExists(bytes32 _id) private view returns (bool) {
-        Book memory existingBook = books[_id];
-        return bytes(existingBook.title).length != 0;
-    }
-
-    modifier validateInputs(string memory _title, uint256 _copies) {
-        require(bytes(_title).length > 0, "Title should not be empty.");
-        require(
-            _checkBookExists(_getId(_title)) == false,
-            "Book already added."
-        );
-        require(_copies > 0, "Copies should be greater than 0.");
-        _;
     }
 }
